2c7b8ba5240cc87ddd734f890584a39e8d3a16b8
minhpho8202 <92845747+minhpho8202@users.noreply.github.com>
2023-11-29 11:03:19 +0700

    'update'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
   controllers: [],
   providers: [],
 })
